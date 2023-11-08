4b5b94840481455c57c465a086f307e838a8eed6
ankit961 <ankit961@gmail.com>
2023-11-08 17:32:42 +0530

    updated cloudbuild.yaml

---
+++ run_migrations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Start the Cloud SQL Proxy
+./cloud_sql_proxy -instances=sensor-reading-404008:asia-south1:sensor=tcp:5432 -ip_address_types=PUBLIC &
+
+# Wait a few seconds to ensure the SQL Proxy has started
+sleep 10
+
+# Run the migrations
+python manage.py db upgrade
+
+# Kill the Cloud SQL Proxy process
+kill $!
