b606085f63044ab8b6fd55761de2bac277553100
ankit961 <ankit961@gmail.com>
2023-11-08 18:32:38 +0530

    updated config

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = "8f7c95ef3e6507753ea648c4659c36f1"
-    SQLALCHEMY_DATABASE_URI = "postgresql://infyulabs:infyulabs123@/sensors?host=/cloudsql/sensor-reading-404008:asia-south1:sensor"
+    SQLALCHEMY_DATABASE_URI = "postgresql://infyulabs:infyulabs123@/sensors?host=/cloud_sql_proxy-instances=sensor-reading-404008:asia-south1:sensor=tcp:5432"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
