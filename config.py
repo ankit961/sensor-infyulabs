9578a9f5dcaf4b3ef1fe7ace316a6c41aedd02bc
ankit961 <ankit961@gmail.com>
2023-11-14 20:13:20 +0530

    flask ap

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = "8f7c95ef3e6507753ea648c4659c36f1"
-    SQLALCHEMY_DATABASE_URI = "postgresql://infyulabs:infyulabs123@/sensors?host=/cloud_sql_proxy-instances=sensor-reading-404008:asia-south1:sensor=tcp:5432"
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///default.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- manage.py
+++ manage.py
@@ -7,9 +7,9 @@ import model
 migrate = Migrate(app, db)
 alembic_config = Config("migrations/alembic.ini")
 
-# alembic_config.set_main_option("sqlalchemy.url", "postgresql://infyulabs:infyulabs123@localhost:5432/sensors")
+alembic_config.set_main_option("sqlalchemy.url", "postgresql://infyulabs:infyulabs123@localhost:5432/sensors")
 # #Production
-alembic_config.set_main_option("sqlalchemy.url", "postgresql://infyulabs:infyulabs123@/sensors?host=/cloud_sql_proxy-instances=sensor-reading-404008:asia-south1:sensor=tcp:5432")
+# alembic_config.set_main_option("sqlalchemy.url", "postgresql://infyulabs:infyulabs123@/sensors?host=/cloud_sql_proxy-instances=sensor-reading-404008:asia-south1:sensor=tcp:5432")
 
 @app.cli.command("db_init")
 def db_init():
